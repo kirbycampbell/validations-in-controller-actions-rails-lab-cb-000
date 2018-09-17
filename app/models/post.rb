57344a48aa6579c3571103ee93630450cf5765af
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-17 19:48:32 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
+  validates :category, presence: true, if :correct_cat?(:category)
+
+  def correct_cat?(cat)
+    if cat === "Non-Fiction" || cat === "Fiction"
+      true
+    else
+      false
+  end
 end
